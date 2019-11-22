a7363d6d9174b3a58ba854e6bc5c9ce0a6f0f890
Mike Bergmann <mike@mdb977.de>
2019-11-22 09:02:51 +0100

    View graphl via graphviz dot example

---
+++ graphmldotviewer.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import argparse
+import sys
+import networkx as nx
+from networkx.drawing.nx_agraph import graphviz_layout
+import matplotlib.pyplot as plt
+
+def main():
+	parser = argparse.ArgumentParser()
+	parser.add_argument('dependencyfile', help='dependency graphml file')
+	args = parser.parse_args()
+
+	path = args.dependencyfile
+
+	try:
+		graph = nx.read_graphml(path)
+		pos = graphviz_layout(graph, prog='dot')
+		nx.draw(graph, pos, with_labels=True)
+		plt.show()
+		return 0
+
+	except Exception as ex:
+		sys.stderr.write('ERROR: ' + str(ex) + '\n')
+		return 1
+
+if __name__ == "__main__":
+	sys.exit(main())
